654f997dffacbe89fce5f2e04a84c16ce469edfa
Cole Stephenson <colestephenson94@gmail.com>
2022-04-18 20:30:21 -0600

    Fix spelling errors 2

--- challenge/pairedChallenge.js
+++ challenge/pairedChallenge.js
@@ -1,8 +1,8 @@
-//I named the file paired.Challenge.js because it is the activity title of what we are doing.
+//I named the file pairedChallenge.js because it is the activity title of what we are doing. .js implies a javascript file.
 var favBook = "The Way of Kings";
 var numFavGenres = 1; // Fantasy books always
 var favBook = true;
-var bestChar = "Kaladin"; // Kaladin is the most compelling proteagonist of any book I've read.
+var bestChar = "Kaladin"; // Kaladin is the most compelling protagonist of any book I've read.
 var avgNumPages = 750; //most books I read are long.
 var badChar = false;
 var bestBooks = ["WoK", "WoT", "NotW", "LoLL", "TS"]; //These are all abbreviated because no one has time for that
